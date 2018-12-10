a1099e1110f38092c8622e4b1fe98d7265fc73c8
santajonas <42831053+santajonas@users.noreply.github.com>
2018-12-10 13:23:51 +0100

    user
    added user

---
+++ src/User.java
@@ -0,0 +1,12 @@
+public class User{
+    UserInfo info;
+    BBR bbrData;
+
+    public UserInfo getInfo() {
+        return info;
+    }
+
+    public BBR getBbrData() {
+        return bbrData;
+    }
+}
