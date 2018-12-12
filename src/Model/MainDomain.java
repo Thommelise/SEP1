098314d8b4132602d7ec2fe16a3823db8eb28155
Thommelise <Thomsen91@outlook.com>
2018-12-12 12:48:37 +0100

    Update MainDomain.java

--- src/Model/MainDomain.java
+++ src/Model/MainDomain.java
@@ -4,7 +4,21 @@ import java.util.ArrayList;
 
 public class MainDomain {
 
+    String username;
+    String password;
+    String address;
+
     private ArrayList<BBR> bbrArrayList = new ArrayList<>();
 
-    public BBR 
+    public User login (String username, String password){
+        this.username = username;
+        this.password = password;
+
+        return login(username, password);
+    }
+
+    public BBR getBbrObject (String address) { this.address = address; return getBbrObject(address);}
+
+    
 }
+
