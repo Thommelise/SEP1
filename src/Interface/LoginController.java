53dce122640c330c6607bc66f1807e315901a007
Thommelise <Thomsen91@outlook.com>
2018-12-13 12:12:41 +0100

    interface

---
+++ src/Interface/LoginController.java
@@ -0,0 +1,18 @@
+
+import Handlers.UserValidationHandler;
+import javafx.event.*;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+
+public class LoginController {
+    @FXML  private TextField user;
+    @FXML  private TextField password;
+    @FXML  private Button loginButton;
+
+
+    
+
+
+};
+
