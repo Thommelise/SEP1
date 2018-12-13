adc39acf4d43aba544e5ad493d6339b865573d4f
Thommelise <Thomsen91@outlook.com>
2018-12-13 18:48:56 +0100

    interface

--- src/Interface/AddNewUserController.java
+++ src/Interface/AddNewUserController.java
@@ -1,9 +1,13 @@
 package Interface;
 
+import Model.BBR;
 import Model.MainDomain;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
 
 import java.awt.*;
+import java.io.IOException;
 
 public class AddNewUserController {
 
@@ -13,10 +17,18 @@ public class AddNewUserController {
     @FXML private TextField username;
     @FXML private TextField role;
     @FXML private TextField password;
+    @FXML AnchorPane rootPane;
 
     @FXML
     void addNewUser (javafx.event.ActionEvent event){
 
-    domain.addUser(name.getText(),emailaddress.getText() ,username.getText() ,role.getText() , ,password.getText() );
+    domain.addUser(name.getText(),emailaddress.getText() ,username.getText() ,role.getText() , domain.getBbrObject(),password.getText() );
+}
+
+@FXML
+    void Home (javafx.event.ActionEvent event) throws IOException {
+
+    AnchorPane superUser = FXMLLoader.load(getClass().getResource("SuperUser.fxml"));
+    rootPane.getChildren().setAll(superUser);
 }
 }
---
+++ src/Interface/ConsumptionController.java
@@ -0,0 +1,17 @@
+package Interface;
+
+import Model.BBR;
+import Model.MainDomain;
+import javafx.fxml.FXML;
+
+public class ConsumptionController {
+
+
+    MainDomain domain = new MainDomain();
+
+    @FXML
+    void ShowConsumption (javafx.event.ActionEvent event){
+
+        System.out.println(domain.toString());
+    }
+}
